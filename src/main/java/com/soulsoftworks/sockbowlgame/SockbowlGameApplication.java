87348d5ae68a5f64b5a912733146d55c20595a01
Jacob Sabella <jacobsabella@yahoo.com>
2022-08-31 16:46:10 -0500

    Initial commit

---
+++ src/main/java/com/soulsoftworks/sockbowlgame/SockbowlGameApplication.java
@@ -0,0 +1,13 @@
+package com.soulsoftworks.sockbowlgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockbowlGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockbowlGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soulsoftworks/sockbowlgame/SockbowlGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulsoftworks.sockbowlgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockbowlGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
