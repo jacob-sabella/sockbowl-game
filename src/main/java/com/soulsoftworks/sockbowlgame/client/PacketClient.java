d35b1e3b595bf508aee8477fe182c65eaf80e21e
Jacob Sabella <jacobsabella@outlook.com>
2025-02-16 08:58:22 -0600

    Fix graphql url

--- src/main/java/com/soulsoftworks/sockbowlgame/client/PacketClient.java
+++ src/main/java/com/soulsoftworks/sockbowlgame/client/PacketClient.java
@@ -10,26 +10,26 @@ import reactor.core.publisher.Mono;
 @Component
 public class PacketClient {
 
-    private final GraphQlClient graphQlClient;
+  private final GraphQlClient graphQlClient;
 
-    public PacketClient(SockbowlQuestionsConfig sockbowlQuestionsConfig) {
-        this.graphQlClient = HttpGraphQlClient.builder()
-                .url(sockbowlQuestionsConfig.getUrl() + "/graphql")
-                .build();
-    }
+  public PacketClient(SockbowlQuestionsConfig sockbowlQuestionsConfig) {
+    this.graphQlClient = HttpGraphQlClient.builder()
+        .url(sockbowlQuestionsConfig.getUrl() + "graphql")
+        .build();
+  }
 
-    /**
-     * Fetches a packet by its ID
-     *
-     * @param packetId The packet ID
-     * @return A Mono emitting the Packet object
-     */
-    public Mono<Packet> getPacketById(String packetId) {
-        String query = "query($id: ID!) { getPacketById(id: $id) { id name } }";
+  /**
+   * Fetches a packet by its ID
+   *
+   * @param packetId The packet ID
+   * @return A Mono emitting the Packet object
+   */
+  public Mono<Packet> getPacketById(String packetId) {
+    String query = "query($id: ID!) { getPacketById(id: $id) { id name } }";
 
-        return graphQlClient.document(query)
-                .variable("id", packetId)
-                .retrieve("getPacketById")
-                .toEntity(Packet.class);
-    }
+    return graphQlClient.document(query)
+        .variable("id", packetId)
+        .retrieve("Packet")
+        .toEntity(Packet.class);
+  }
 }
