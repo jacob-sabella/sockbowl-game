4352dca8582ec316c710c79ba88e7c5005797a29
Jacob Sabella <jacobsabella@outlook.com>
2025-08-09 10:08:59 -0500

    chore: remove old unused CI files

--- docker-image-push.sh
+++
@@ -1,2 +0,0 @@
-docker tag sockbowl-game:0.0.1-SNAPSHOT 339712961919.dkr.ecr.us-east-1.amazonaws.com/sockbowl-game:latest
-docker push 339712961919.dkr.ecr.us-east-1.amazonaws.com/sockbowl-game:latest
