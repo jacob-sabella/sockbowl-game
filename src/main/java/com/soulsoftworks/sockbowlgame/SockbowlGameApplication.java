8fb04c6b12cee4ccfaa2eb5b64e51a15b0f4b1dc
Jacob Sabella <jacobsabella@outlook.com>
2025-12-29 11:36:09 -0600

    Exclude configurations

--- src/main/java/com/soulsoftworks/sockbowlgame/SockbowlGameApplication.java
+++ src/main/java/com/soulsoftworks/sockbowlgame/SockbowlGameApplication.java
@@ -3,8 +3,14 @@ package com.soulsoftworks.sockbowlgame;
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 @EnableRedisDocumentRepositories
@@ -13,12 +19,20 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class SockbowlGameApplication {
 
 	public static void main(String[] args) {
+		String authEnabled = System.getenv("SOCKBOWL_AUTH_ENABLED");
+		boolean isAuthDisabled = authEnabled == null || authEnabled.equals("false");
+
 		SpringApplication app = new SpringApplication(SockbowlGameApplication.class);
 
-		// Conditionally exclude autoconfiguration based on auth.enabled property
-		String authEnabled = System.getenv("SOCKBOWL_AUTH_ENABLED");
-		if (authEnabled == null || authEnabled.equals("false")) {
-			app.setAdditionalProfiles("no-auth");
+		// Programmatically exclude autoconfiguration when auth is disabled
+		if (isAuthDisabled) {
+			Set<String> exclusions = new HashSet<>();
+			exclusions.add(DataSourceAutoConfiguration.class.getName());
+			exclusions.add(HibernateJpaAutoConfiguration.class.getName());
+			exclusions.add(OAuth2ClientAutoConfiguration.class.getName());
+			app.setDefaultProperties(java.util.Map.of(
+				"spring.autoconfigure.exclude", String.join(",", exclusions)
+			));
 		}
 
 		app.run(args);
